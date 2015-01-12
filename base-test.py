7fcb47b014e8e9fd30454a37a52a964a970c37ac
Andrzej Zaborowski <andrew.zaborowski@intel.com>
2015-01-12 19:04:30 +0100

    Implement Base library for bluetooth and other bases to use.
    Also rewrite base-test.py to use the Base library instead of doing
    all the manual work.  There's a slight regression as currently
    base-triggered PUBLISH messages are not sent, will come in the next
    commit.

--- base-test.py
+++ base-test.py
@@ -16,184 +16,37 @@
 #     every 20 seconds indefinitely.
 #   * a relay that controls the light in the room.
 
-import socket
-import config
-import select
 import time
-import json
-import base_server
-import sensorino
 import sys
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(config.base_server_address)
+import base_lib
 
 node_addr = 10
-base_addr = 0
 
-svc_manager_svcs = [ 0 ]
 relay_svcs = [ 2 ]
 switch_svcs = [ 3 ]
 
-switch = [ True for svc in switch_svcs ]
-relay = [ True for svc in relay_svcs ]
-
-def handle_20s_timeout():
-	global s, switch, node_addr, base_addr, switch_svcs
-	switch[0] = not switch[0]
-	obj = {
-		'from': node_addr,
-		'to': base_addr,
-		'type': 'publish',
-		'serviceId': switch_svcs[0],
-		'switch': switch[0],
-	}
-	s.send(json.dumps(obj).encode('utf8'))
-
-def handle_input(msg):
-	global s, switch, relay, node_addr, base_addr, \
-		svc_manager_svcs, relay_svcs, switch_svcs
-
-	try:
-		obj = sensorino.message_dict(json.loads(msg))
-	except:
-		ret = {
-			'error': 'syntaxError',
-		}
-		s.send(json.dumps(ret).encode('utf8'))
-		return
-
-	if 'type' not in obj:
-		ret = {
-			'error': 'noType',
-		}
-		s.send(json.dumps(ret).encode('utf8'))
-		return
-
-	ok = 1
-	for prop in [ 'from', 'serviceid' ]: # TODO: fuller checks
-		if prop in obj and not isinstance(obj[prop], int):
-			ok = 0
-	for prop in [ 'switch' ]:
-		if prop in obj and not isinstance(obj[prop], bool):
-			ok = 0
-	if 'dataType' in obj and (not isinstance(obj['dataType'], basestring) or
-			obj['dataType'] not in sensorino.known_datatypes_dict):
-		ok = 0
-
-	if 'to' not in obj or not isinstance(obj['to'], int) or \
-			obj['type'] not in \
-			[ 'publish', 'set', 'request', 'err' ] or not ok:
-		ret = {
-			'error': 'structError',
-		}
-		s.send(json.dumps(ret).encode('utf8'))
-		return
-
-	if obj['to'] != node_addr:
-		ret = {
-			'error': 'xmitError',
-		}
-		s.send(json.dumps(ret).encode('utf8'))
-		return
-
-	all_svcs = svc_manager_svcs + relay_svcs + switch_svcs
-	if 'serviceId' not in obj or obj['serviceId'] not in all_svcs:
-		ret = {
-			'from': node_addr,
-			'to': base_addr,
-			'type': 'err',
-			'dataType': 'ServiceId',
-		}
-		s.send(json.dumps(ret).encode('utf8'))
-		return
-
-	permitted_types = [ 'request' ]
-	if obj['serviceId'] in relay_svcs:
-		permitted_types.append('set')
-
-	if obj['type'] not in permitted_types:
-		ret = {
-			'from': node_addr,
-			'to': base_addr,
-			'type': 'err',
-			'serviceId': obj['serviceId'],
-		}
-		s.send(json.dumps(ret).encode('utf8'))
-		return
-
-	if obj['serviceId'] in svc_manager_svcs:
-		ret = {
-			'from': node_addr,
-			'to': base_addr,
-			'type': 'publish',
-			'serviceId': all_svcs,
-		}
-		s.send(json.dumps(ret).encode('utf8'))
-		return
-
-	if obj['type'] == 'request' and 'dataType' in obj and \
-			obj['dataType'].lower() not in [ 'datatype', 'switch' ]:
-		ret = {
-			'from': node_addr,
-			'to': base_addr,
-			'type': 'err',
-			'serviceId': obj['serviceId'],
-			'dataType': 'DataType',
-		}
-		s.send(json.dumps(ret).encode('utf8'))
-		return
-
-	if obj['type'] == 'request' and 'dataType' in obj and \
-			obj['dataType'].lower() == 'datatype':
-		if obj['serviceId'] in relay_svcs:
-			# Relay service accepts one value and publishes none
-			cnt = [ 0, 1 ]
-		else:
-			# Switch service publish one value and accepts none
-			cnt = 1
-		ret = {
-			'from': node_addr,
-			'to': base_addr,
-			'type': 'publish',
-			'serviceId': obj['serviceId'],
-			'count': cnt,
-			'dataType': 'Switch',
-		}
-		s.send(json.dumps(ret).encode('utf8'))
-		return
-
-	# Now we know the request has no DataType or DataType is Switch
-	if obj['type'] == 'request':
-		if obj['serviceId'] in relay_svcs:
-			val = relay[relay_svcs.index(obj['serviceId'])]
-		else:
-			val = switch[switch_svcs.index(obj['serviceId'])]
-		ret = {
-			'from': node_addr,
-			'to': base_addr,
-			'type': 'publish',
-			'serviceId': obj['serviceId'],
-			'switch': val,
-		}
-		s.send(json.dumps(ret).encode('utf8'))
-		return
-
-	# Now we know type is "set" and ServiceId is relay's
-	idx = relay_svcs.index(obj['serviceId'])
-	prev_relay = relay[idx]
-	if 'switch' in obj:
-		relay[idx] = obj['switch']
-	else:
-		relay[idx] = not relay[idx]
-
-	if relay[idx] != prev_relay:
-		if relay[idx]:
+class relay_channel(base_lib.base_channel):
+	def set_value(self, val):
+		base_lib.base_channel.set_value(self, val)
+		if val:
 			sys.stderr.write('It\'s bright again.\n')
 		else:
 			sys.stderr.write('It\'s dark now.\n')
 
-parse_state = base_server.obj_parser()
+node = base_lib.base_create_node(node_addr)
+
+switches = [ node.create_service(svc_id).create_channel('switch', False, True) \
+	for svc_id in switch_svcs ]
+
+for svc_id in relay_svcs:
+	node.create_service(svc_id).add_channel(relay_channel('switch', \
+			True, True))
+
+def handle_20s_timeout():
+	switches[0].set_value(not switches[0].get_value())
+
+base_lib.base_init()
 last_ts = time.time()
 while 1:
 	now = time.time()
@@ -204,16 +57,10 @@ while 1:
 		continue
 
 	try:
-		r, w, e = select.select([ s ], [], [ s ], timeout)
+		base_lib.base_run(timeout)
 	except KeyboardInterrupt:
 		break
-	if len(e):
+	except base_lib.BaseDisconnect:
 		break
-	if not len(r):
-		continue
 
-	for chr in s.recv(8192).decode('utf-8'):
-		obj = parse_state.handle_char(chr)
-		if obj is not None:
-			handle_input(obj)
-s.close()
+base_lib.base_done()
---
+++ base_lib.py
@@ -0,0 +1,397 @@
+# Sensorino smarthome server
+#
+# Author: Andrew Zaborowski <andrew.zaborowski@intel.com>
+#
+# This software is provided under the 3-clause BSD license.
+#
+# Utilities to facilities writing new Base scripts.  When a user wants
+# to connect smart devices to the Sensorino server, which talk an as yet
+# unsupported protocol or Radio Access Technology, they will need to
+# create a gateway or adapter script that interfaces their radio stack
+# to the Sensorino server.  For that they can use the python API provided
+# by this library instead of having to translate each message between the
+# Sensorino native format and that of the target technology.
+
+import socket
+import select
+import json
+import collections
+
+import config
+import base_server
+import sensorino
+
+svc_manager_id = 0
+
+class base_node(object):
+	def __init__(self, addr):
+		self.addr = addr
+		self.services = {}
+		self.name = None
+		self.add_service(base_service_manager(self))
+
+	def get_addr(self):
+		return self.addr
+
+	def set_name(self, name):
+		self.name = name
+
+		# TODO: if comms initialised,
+		# send a message with svc_id == _name to server?
+
+	def create_service(self, *args):
+		svc = base_service(*args)
+
+		self.add_service(svc)
+
+		return svc
+
+	def add_service(self, svc):
+		self.services[svc.get_id()] = svc
+
+		# TODO: if comms initialised,
+		# send a "discovery" message from the node's Service Manager?
+
+class base_service(object):
+	def __init__(self, svc_id):
+		if not isinstance(svc_id, int):
+			raise Exception('ServiceId must be an int, sorry')
+		self.svc_id = svc_id
+		self.channels = {}
+		self.name = None
+
+	def get_id(self):
+		return self.svc_id
+
+	def set_name(self, name):
+		self.name = name
+
+		# TODO: if comms initialised,
+		# send a message with datatype == _name to server?
+
+	def create_channel(self, *args):
+		chan = base_channel(*args)
+
+		self.add_channel(chan)
+
+		return chan
+
+	def add_channel(self, channel):
+		if channel.get_type() not in self.channels:
+			self.channels[channel.get_type()] = ( [], [] )
+
+		idx = 0
+		if channel.is_actuator():
+			idx = 1
+		self.channels[channel.get_type()][idx].append(channel)
+
+	def get_chan_values(self, types):
+		ret = {}
+		for type in types:
+			ret[type] = [ \
+				chan.get_value() for chan in \
+				self.channels[type][0] + \
+				self.channels[type][1] \
+			]
+		return ret
+
+	def get_values(self, types):
+		chan_types = [ type for type in types if type != 'dataType' ]
+		ret = self.get_chan_values(chan_types)
+
+		if 'dataType' in types:
+			publish_types = []
+			accept_types = []
+			for type in self.channels:
+				p_chans, a_chans = self.channels[type]
+				publish_types += [ type for ch in p_chans ]
+				accept_types += [ type for ch in a_chans ]
+			ret['dataType'] = publish_types + accept_types
+			ret['count'] = [ len(publish_types) ]
+			if len(accept_types):
+				ret['count'].append(len(accept_types))
+
+		return ret
+
+	def set_values(self, value_map):
+		for type, num in value_map:
+			val = value_map[( type, num )]
+			# Might want to check if new != old here.
+			self.channels[type][1][num].set_value(val)
+
+class base_channel(object):
+	def __init__(self, type, writable, init_val=None):
+		type = sensorino.datatype_to_name(type)
+		self.type = type
+		self.writable = writable
+		self.name = None
+		self.value = init_val
+
+	def get_type(self):
+		return self.type
+
+	def is_actuator(self):
+		return self.writable
+
+	def set_name(self, name):
+		self.name = name
+
+		# TODO: if comms initialised,
+		# send a message with datatype == _chan_names to server?
+
+	def set_value(self, new_value):
+		self.value = new_value
+
+		# For writable (actuator) channels this should not be
+		# called by client code because those channels are assumed
+		# to never publish by themselves, only on server's request.
+		#
+		# Overload this function to add a handler for a single
+		# channel, or overload the service's set_values function
+		# to handle all the new values at once.
+
+		# For read-only (sensor) channels, client code should either
+		# call this, or the service's set_values method to publish
+		# a new value.
+		if not self.writable:
+			# TODO: actually send a message to the base
+			pass
+
+	def get_value(self):
+		return self.value
+
+class base_service_manager(base_service):
+	def __init__(self, node):
+		super(base_service_manager, self).__init__(svc_manager_id)
+		self.create_channel('serviceId', False)
+		self.node = node
+
+	def get_chan_values(self, types):
+		svcs = self.node.services.keys()
+		svcs.remove(self.get_id())
+		return { 'serviceId': svcs }
+
+def base_create_node(*args):
+	node = base_node(*args)
+
+	base_add_node(node)
+
+	return node
+
+# All simulated nodes
+nodes = {}
+
+def base_add_node(node):
+	global nodes
+	nodes[node.get_addr()] = node
+
+base_addr = 0
+
+def base_send_obj(obj):
+	global s
+	for field in obj:
+		if isinstance(obj[field], basestring) or not \
+				isinstance(obj[field], collections.Sequence):
+			continue
+		if len(obj[field]) == 1:
+			obj[field] = obj[field][0]
+	s.send(json.dumps(obj).encode('utf8'))
+
+def base_handle_set(node, svc, msg):
+	channels = {}
+	for prop in msg:
+		type = prop.lower()
+		if type in sensorino.known_datatypes_dict:
+			type = sensorino.datatype_to_name(prop)
+
+		vallist = sensorino.valuelist_from_msg(msg, type)
+		svc_channels = []
+		if type in svc.channels:
+			svc_channels = svc.channels[type][1]
+
+		# TODO: 'count'-typed properties in a SET may be used as an
+		# argument to the write operation if accompanied by other
+		# properties.  Not an error.
+
+		# Note: on a Sensorino node unrecognized or excess values in
+		# a SET message will normally not generate any error and will
+		# simply be ignored.
+
+		if len(vallist) > len(svc_channels):
+			base_send_obj({
+					'from': node.get_addr(),
+					'to': base_addr,
+					'type': 'err',
+					'serviceId': svc.get_id(),
+					'dataType': prop,
+				})
+			return
+
+		for chan_num, val in enumerate(vallist):
+			channels[( type, chan_num )] = val
+
+	# Special case: if a SET message contains no values and is
+	# addressed at a service with one actuator channel of a boolean type,
+	# flip that channel's value.
+	if not len(channels) and 'switch' in svc.channels and \
+			len(svc.channels['switch'][1]):
+		channels[( 'switch', 0 )] = \
+			not svc.channels['switch'][1][0].get_value()
+	elif not len(channels):
+		base_send_obj({
+				'from': node.get_addr(),
+				'to': base_addr,
+				'type': 'err',
+				'serviceId': svc.get_id(),
+			})
+		return
+
+	svc.set_values(channels)
+
+def base_handle_request(node, svc, msg):
+	if 'dataType' in msg:
+		channels = sensorino.valuelist_from_msg(msg, 'dataType')
+		del msg['dataType']
+	else:
+		channels = svc.channels.keys()
+
+	if len(msg):
+		# TODO: 'count'-typed properties in a SET may be used as an
+		# argument to the read operation if accompanied by other
+		# properties.  Not an error.
+
+		# Note: on a Sensorino node unrecognized or excess values in
+		# a REQUEST message will normally not generate any error and
+		# will simply be ignored.
+
+		base_send_obj({
+				'from': node.get_addr(),
+				'to': base_addr,
+				'type': 'err',
+				'serviceId': svc.get_id(),
+				'dataType': msg.keys()[0],
+				})
+		return
+
+	ret = svc.get_values(channels)
+	ret['from'] = node.get_addr()
+	ret['to'] = base_addr
+	ret['type'] = 'publish'
+	ret['serviceId'] = [ svc.get_id() ] + ret.get('serviceId', [])
+	if len(ret['serviceId']) == 1:
+		ret['serviceId'] = ret['serviceId'][0]
+	base_send_obj(ret)
+
+def base_handle_input(msg):
+	global nodes, svc_manager_svcs
+
+	# Check message parses as JSON
+	try:
+		obj = sensorino.message_dict(json.loads(msg))
+	except:
+		base_send_obj({ 'error': 'syntaxError' })
+		return
+
+	# Check if type is present
+	if 'type' not in obj:
+		base_send_obj({ 'error': 'noType' })
+		return
+	msg_type = obj['type']
+
+	# General message structure check
+	ok = 1
+	for prop in [ 'from' ]:
+		if prop in obj and not isinstance(obj[prop], int):
+			ok = 0
+	for prop in obj:
+		if prop not in sensorino.known_datatypes_dict:
+			continue
+		type = sensorino.datatype_to_name(prop)
+		try:
+			vallist = sensorino.valuelist_from_msg(obj, prop)
+			sensorino.valuelist_validate_types(vallist, type)
+		except:
+			ok = 0
+
+	if 'to' not in obj or msg_type not in \
+			[ 'publish', 'set', 'request', 'err' ] or not ok:
+		base_send_obj({ 'error': 'structError' })
+		return
+
+	# Check if addressed node exists
+	if obj['to'] not in nodes:
+		base_send_obj({ 'error': 'xmitError' })
+		return
+
+	# Check if service exists
+	node = nodes[obj['to']]
+
+	main_svc_id = None
+	if 'serviceId' in obj:
+		svc_ids = sensorino.valuelist_from_msg(obj, 'serviceId')
+		main_svc_id = svc_ids.pop(0)
+
+	if main_svc_id not in node.services:
+		base_send_obj({
+				'from': node.get_addr(),
+				'to': base_addr,
+				'type': 'err',
+				'dataType': 'ServiceId',
+			})
+		return
+
+	svc = node.services[main_svc_id]
+
+	# Message type check
+	if msg_type not in [ 'request', 'set' ]:
+		base_send_obj({
+				'from': node.get_addr(),
+				'to': base_addr,
+				'type': 'err',
+				'serviceId': main_svc_id,
+			})
+		return
+
+	# Pass the message down without the fields already handled
+	for prop in [ 'to', 'from', 'serviceId', 'type' ]:
+		obj.pop(prop, None)
+	if len(svc_ids):
+		obj['serviceId'] = svc_ids
+
+	if msg_type == 'set':
+		base_handle_set(node, svc, obj)
+	else:
+		base_handle_request(node, svc, obj)
+
+# Objectify?
+
+s = None
+parse_state = None
+
+def base_init():
+	global s, parse_state
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect(config.base_server_address)
+
+	parse_state = base_server.obj_parser()
+
+class BaseDisconnect(Exception):
+	pass
+
+def base_run(timeout):
+	global s, parse_state
+
+	r, w, e = select.select([ s ], [], [ s ], timeout)
+	if len(e):
+		raise BaseDisconnect()
+	if not len(r):
+		return
+
+	for chr in s.recv(8192).decode('utf-8'):
+		obj = parse_state.handle_char(chr)
+		if obj is not None:
+			base_handle_input(obj)
+
+def base_done():
+	global s
+	s.close()
