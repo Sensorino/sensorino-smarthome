92cc3469edf278ca8a82e8de1d2a6d89698a6475
Andrzej Zaborowski <andrew.zaborowski@intel.com>
2015-01-17 04:36:00 +0100

    Half-functional Bluetooth Low-Energy base.
    Requires very recent 5.1x or 5.2x bluez version, dbus-python and that
    bluetoothd is launched with -E (may require editing the init script).
    
    Full implementation is blocked by Bluez' support for GATT notifications --
    sensor channels won't work without that, except with polling.  We can
    use polling in the python script for now, but that's not done yet.
    Actuator support isn't done either.

---
+++ base-ble-dbus.py
@@ -0,0 +1,423 @@
+#! /usr/bin/python
+#
+# Sensorino smarthome server
+#
+# Author: Andrew Zaborowski <andrew.zaborowski@intel.com>
+#
+# This software is provided under the 3-clause BSD license.
+#
+# This is a gateway script for the Sensorino Server to be able to
+# discover and use some Bluetooth Low-Energy devices.  When run, it'll
+# connect to the server and create a Sensorino node for each supported
+# Bluetooth device, with a Sensorino data channel for each supported
+# characteristic, grouped into Sensorino services by their membership
+# within GATT services.
+
+import dbus
+import dbus.mainloop.glib
+import gobject
+import bluezutils
+
+import base_lib
+
+dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+bus = dbus.SystemBus()
+
+device_queue = []
+
+class bt_device(object):
+	devices = {}
+
+	def __init__(self, path):
+		self.path = path
+		self.discovered = False
+		self.known_characteristic_paths = {}
+		self.characteristics = {}
+
+		self.sensorino_node = None
+		self.sensorino_services = {}
+
+		obj = bus.get_object(bluezutils.SERVICE_NAME, self.path)
+		props = dbus.Interface(obj, 'org.freedesktop.DBus.Properties')
+		self.addr = props.Get(bluezutils.DEVICE_INTERFACE, 'Address')
+
+		# State values:
+		#  queued:  connection attempt scheduled when the time comes
+		#  busy:    currently connected to or being connected to
+		#  unknown: not queued, may be out-of-range.  In this case
+		#		self.known_characteristic_paths
+		#		being non-empty indicates that we want to
+		#		connect to this device when there's an
+		#		indication that it may be online
+		self.state = 'unknown'
+
+		# Auto-disconnect when this expires
+		self.device_busy_timeout = None
+
+		self.devices[path] = self
+
+	def queue(self):
+		global device_queue, run_queue
+		if self.state != 'unknown':
+			return
+		self.state = 'queued'
+		if self not in device_queue:
+			empty = not device_queue
+			device_queue.append(self)
+			if empty:
+				gobject.idle_add(run_queue)
+
+	def dequeue(self):
+		global device_queue
+		if self.state == 'queued':
+			self.state = 'unknown'
+		if self in device_queue:
+			device_queue.remove(self)
+
+	def connect(self):
+		global device_queue, run_queue
+		if self.state == 'busy':
+			return
+		self.state = 'busy'
+		if self in device_queue:
+			device_queue.remove(self)
+
+		obj = bus.get_object(bluezutils.SERVICE_NAME, self.path)
+		dev = dbus.Interface(obj, bluezutils.DEVICE_INTERFACE)
+		props = dbus.Interface(obj, 'org.freedesktop.DBus.Properties')
+
+		paired = props.Get(bluezutils.DEVICE_INTERFACE, 'Paired')
+
+		name = str(self.addr)
+
+		# Tell bluez to connect to this device for 10s, assume it'll
+		# manage to complete all the basic discovery in that time and
+		# call relevant listeners, then .Disconnect if it doesn't do
+		# that earlier automatically.  When we're pairing it seems the
+		# basic discovery is finished at the moment .Pair() returns
+		# but we can't rely on that, .Pair() will fail if .Paired is
+		# True so we must use .Connect() meaning that the discovery
+		# is asynchronous.
+		try:
+			if self.discovered or paired:
+				print('Connecting to ' + name)
+				dev.Connect()
+			else:
+				print('Pairing with ' + name + ' for scanning')
+				dev.Pair()
+		except Exception as e:
+			print('Could not connect to ' + name + ': ' + str(e))
+			self.state = 'unknown'
+			#gobject.idle_add(run_queue)
+			return
+
+		self.device_busy_timeout = \
+			gobject.timeout_add(10000, self.to_disconnect, dev)
+
+	def to_disconnect(self, dev):
+		self.device_busy_timeout = None
+		self.disconnect()
+		return False
+
+	def disconnect(self, dev):
+		if self.device_busy_timeout is not None:
+			gobject.source_remove(self.device_busy_timeout)
+			self.device_busy_timeout = None
+
+		dev.Disconnect()
+
+	def connected(self):
+		global device_queue
+		self.state = 'busy'
+		if self in device_queue:
+			device_queue.remove(self)
+
+		# If this device acutally has characteristics that we're
+		# interested in, we change our mind and will not auto-
+		# disconnect.
+		if self.known_characteristic_paths and \
+				self.device_busy_timeout is not None:
+			gobject.source_remove(self.device_busy_timeout)
+			self.device_busy_timeout = None
+
+		for path in self.known_characteristic_paths:
+			if path not in self.characteristics:
+				characteristic_class = \
+					self.known_characteristic_paths[path]
+				self.characteristics[path] = \
+					characteristic_class(path, self)
+			self.characteristics[path].active()
+
+	def disconnected(self):
+		global device_queue
+		prev_state = self.state
+		self.state = 'unknown'
+
+		if self in device_queue:
+			device_queue.remove(self)
+
+		name = str(self.addr)
+
+		if prev_state == 'busy':
+			gobject.idle_add(run_queue)
+
+			print('Disconnected from ' + name)
+
+			for path in self.characteristics:
+				self.characteristics[path].inactive()
+
+	def handle_characteristic(self, path, cls):
+		if path in self.known_characteristic_paths:
+			return
+
+		self.known_characteristic_paths[path] = cls
+		if self.state == 'unknown':
+			self.queue()
+		elif self.state == 'busy':
+			# Make sure we instantiate this characteristic
+			self.connected()
+
+	@staticmethod
+	def busy():
+		for path in bt_device.devices:
+			if bt_device.devices[path].state == 'busy':
+				return True
+		return False
+
+def run_queue():
+	while device_queue and not bt_device.busy():
+		device_queue[0].connect()
+	return False
+
+def device_change(path, changed):
+	if path in bt_device.devices:
+		dev = bt_device.devices[path]
+	else:
+		dev = bt_device(path)
+
+	if 'Connected' in changed:
+		obj = bus.get_object(bluezutils.SERVICE_NAME, path)
+		props = dbus.Interface(obj, 'org.freedesktop.DBus.Properties')
+
+		if props.Get(bluezutils.DEVICE_INTERFACE, 'Connected'):
+			dev.connected()
+		else:
+			dev.disconnected()
+
+	if not dev.discovered and ('Paired' in changed or
+			'Connected' in changed):
+		obj = bus.get_object(bluezutils.SERVICE_NAME, path)
+		props = dbus.Interface(obj, 'org.freedesktop.DBus.Properties')
+		paired = props.Get(bluezutils.DEVICE_INTERFACE, 'Paired')
+		connected = props.Get(bluezutils.DEVICE_INTERFACE, 'Connected')
+
+		# Must test both Connected and Paired because bluetoothd will
+		# remove all the service, characteristic, etc. data after some
+		# time from last activity on a device, but will keep Paired
+		# set to True.  Neither Paired nor Connected alone set to True
+		# nor populated UUIDs mean that the sub-objects are present.
+		if connected and paired:
+			dev.discovered = True
+
+	if dev.known_characteristic_paths or not dev.discovered:
+		dev.queue()
+	else:
+		dev.dequeue()
+
+# Services we know how to handle
+known_characteristics = {}
+
+# This is currently only called when the object/interface is created
+def characteristic_change(path, changed):
+	global known_characteristics
+
+	obj = bus.get_object(bluezutils.SERVICE_NAME, path)
+	props = dbus.Interface(obj, 'org.freedesktop.DBus.Properties')
+
+	# TODO: call a function to first check UUID and then also go
+	# through all associated descriptors and figure out based on them
+	# if we could possibly support the characteristic...  If the device
+	# is disconnected though, we'd need to queue it and remember to
+	# re-check all the characteristics when we can read their
+	# descriptors.
+	uuid = props.Get(bluezutils.GATT_CHAR_INTERFACE, 'UUID')
+	if uuid not in known_characteristics:
+		return
+
+	svc_path = props.Get(bluezutils.GATT_CHAR_INTERFACE, 'Service')
+	svc_obj = bus.get_object(bluezutils.SERVICE_NAME, svc_path)
+	svc_props = dbus.Interface(svc_obj, 'org.freedesktop.DBus.Properties')
+
+	dev_path = svc_props.Get(bluezutils.GATT_SVC_INTERFACE, 'Device')
+	# Raise an exception if device doesn't exist
+	dev = bt_device.devices[dev_path]
+
+	dev.handle_characteristic(path, known_characteristics[uuid])
+
+class bt_base_service(base_lib.base_service):
+	def set_values(self, value_map):
+		super(bt_base_service, self).set_values(value_map)
+
+class bt_characteristic(object):
+	def __init__(self, path, dev):
+		self.path = path
+		self.dev = dev
+
+		# Derive the Sensorino service ID from GATT parent service
+		# for this characteristic
+		self.obj = bus.get_object(bluezutils.SERVICE_NAME, self.path)
+		self.props = dbus.Interface(self.obj,
+				'org.freedesktop.DBus.Properties')
+		self.char = dbus.Interface(self.obj,
+				bluezutils.GATT_CHAR_INTERFACE)
+
+		svc_path = self.props.Get(bluezutils.GATT_CHAR_INTERFACE,
+				'Service')
+		short_id = str(svc_path)[-4:]
+		# Use the service handle but reserve the IDs 0..9
+		service_id = int(short_id, 16) + 10
+
+		if dev.sensorino_node is None:
+			dev.sensorino_node = base_lib.base_create_node(dev.addr)
+
+		if service_id not in dev.sensorino_services:
+			svc = bt_base_service(service_id)
+			dev.sensorino_services[service_id] = svc
+			dev.sensorino_node.add_service(svc)
+
+		self.service = dev.sensorino_services[service_id]
+
+		# TODO: figure out how to make the channel order in the
+		# Sensorino service independent of the characteristic
+		# discovery order
+
+		# TODO: also make sure we're flexible enough to allow a
+		# subclass to create just one Sensorino channel from
+		# multiple characteristics
+
+		self.channels = []
+		self.is_active = False
+
+		self.props.connect_to_signal('PropertiesChanged',
+				self.properties_changed)
+
+	def active(self):
+		if self.is_active:
+			return
+		self.is_active = True
+
+		# TODO: check if we have any read-only channels registered
+
+		try:
+			self.char.StartNotify()
+		except Exception as e:
+			print('Characteristic StartNotify() failed: ' + str(e))
+		try:
+			val = self.char.ReadValue()
+		except Exception as e:
+			print('Characteristic ReadValue() failed: ' + str(e))
+			return
+
+		self.handler(val)
+
+	def inactive(self):
+		if not self.is_active:
+			return
+		self.is_active = False
+
+		# TODO: check if we have any read-only channels registered
+
+		try:
+			self.char.StopNotify()
+		except Exception as e:
+			print('Characteristic StopNotify() failed: ' + str(e))
+
+		self.publish_values(*[ None for chan in self.channels ])
+
+	def properties_changed(self, interface, changed, invalidated):
+		if interface == bluezutils.GATT_CHAR_INTERFACE and \
+				'Value' in changed:
+			self.handler(changed['Value'])
+
+	def publish_values(self, *args):
+		value_map = {}
+		for i, chan in enumerate(self.channels):
+			chan_num = chan.get_chan_num(self.service)
+			value_map[chan.type, chan_num] = args[i]
+		self.service.publish_values(value_map)
+
+# Sensor Tag characteristics
+
+class sensor_tag_simple_key_service_char(bt_characteristic):
+	uuid = '0000ffe1-0000-1000-8000-00805f9b34fb'
+
+	def __init__(self, path, dev):
+		super(sensor_tag_simple_key_service_char, self). \
+			__init__(path, dev)
+
+		self.channels = [
+			self.service.create_channel('switch', False),
+			self.service.create_channel('switch', False),
+		]
+		self.channels[0].set_name('Left button')
+		self.channels[1].set_name('Right button')
+
+	def handler(self, val):
+		if not self.is_active:
+			return
+
+		self.publish_values((val[0] >> 0) & 1, (val[0] >> 1) & 1)
+
+char_classes = [
+	sensor_tag_simple_key_service_char,
+]
+for char_class in char_classes:
+	known_characteristics[char_class.uuid] = char_class
+
+adapter = bluezutils.find_adapter(bus)
+
+# Connect the D-Bus main loop with the base_lib main loop
+base_lib.base_init(adapter.object_path)
+def base_run(s, *args):
+	base_lib.base_run(0)
+	return True
+gobject.io_add_watch(base_lib.s,
+		gobject.IO_IN | gobject.IO_ERR | gobject.IO_HUP, base_run)
+
+# Make sure adapter is powered on (UP in hciconfig)
+adapter_props = dbus.Interface(adapter, 'org.freedesktop.DBus.Properties')
+adapter_props.Set(bluezutils.ADAPTER_INTERFACE, 'Powered', dbus.Boolean(True))
+
+# Subscribe to all signals that could indicate a device was just detected
+bluezutils.device_listeners.append(device_change)
+
+bluezutils.characteristic_listeners.append(characteristic_change)
+
+bluezutils.init_listeners(bus)
+
+# Also process devices that have been discovered earlier
+# TODO: cmdline option to skip this
+for path, interfaces in bluezutils.get_managed_objects(bus).iteritems():
+	if bluezutils.DEVICE_INTERFACE in interfaces:
+		device_change(path, interfaces[bluezutils.DEVICE_INTERFACE])
+
+for path, interfaces in bluezutils.get_managed_objects(bus).iteritems():
+	if bluezutils.GATT_CHAR_INTERFACE in interfaces:
+		characteristic_change(path,
+				interfaces[bluezutils.GATT_CHAR_INTERFACE])
+
+try:
+	adapter.StopDiscovery()
+except:
+	pass
+adapter.StartDiscovery()
+
+mainloop = gobject.MainLoop()
+try:
+	mainloop.run()
+except KeyboardInterrupt as e:
+	pass
+except base_lib.BaseDisconnect as e:
+	pass
+finally:
+	base_lib.base_done()
---
+++ bluezutils.py
@@ -0,0 +1,91 @@
+'''test/bluezutils.py, test/test-discovery.py from the bluez source tree with
+a few changes'''
+import dbus
+
+SERVICE_NAME = "org.bluez"
+ADAPTER_INTERFACE = SERVICE_NAME + ".Adapter1"
+DEVICE_INTERFACE = SERVICE_NAME + ".Device1"
+GATT_SVC_INTERFACE = SERVICE_NAME + ".GattService1"
+GATT_CHAR_INTERFACE = SERVICE_NAME + ".GattCharacteristic1"
+GATT_DESC_INTERFACE = SERVICE_NAME + ".GattDescriptor1"
+
+def get_managed_objects(bus):
+	manager = dbus.Interface(bus.get_object("org.bluez", "/"),
+				"org.freedesktop.DBus.ObjectManager")
+	return manager.GetManagedObjects()
+
+def find_adapter(bus, pattern=None):
+	return find_adapter_in_objects(bus, get_managed_objects(bus), pattern)
+
+def find_adapter_in_objects(bus, objects, pattern=None):
+	for path, ifaces in objects.iteritems():
+		adapter = ifaces.get(ADAPTER_INTERFACE)
+		if adapter is None:
+			continue
+		if not pattern or pattern == adapter["Address"] or \
+							path.endswith(pattern):
+			obj = bus.get_object(SERVICE_NAME, path)
+			return dbus.Interface(obj, ADAPTER_INTERFACE)
+	raise Exception("Bluetooth adapter not found")
+
+def find_device(bus, device_address, adapter_pattern=None):
+	return find_device_in_objects(bus, get_managed_objects(bus),
+			device_address, adapter_pattern)
+
+def find_device_in_objects(bus, objects, device_address, adapter_pattern=None):
+	path_prefix = ""
+	if adapter_pattern:
+		adapter = find_adapter_in_objects(bus, objects, adapter_pattern)
+		path_prefix = adapter.object_path
+	for path, ifaces in objects.iteritems():
+		device = ifaces.get(DEVICE_INTERFACE)
+		if device is None:
+			continue
+		if (device["Address"] == device_address and
+						path.startswith(path_prefix)):
+			obj = bus.get_object(SERVICE_NAME, path)
+			return dbus.Interface(obj, DEVICE_INTERFACE)
+
+	raise Exception("Bluetooth device not found")
+
+device_listeners = []
+service_listeners = []
+characteristic_listeners = []
+
+def init_listeners(bus):
+	bus.add_signal_receiver(interfaces_added,
+			dbus_interface = "org.freedesktop.DBus.ObjectManager",
+			signal_name = "InterfacesAdded")
+
+	# TODO: also listen for property changes on the other interfaces
+	bus.add_signal_receiver(properties_changed,
+			dbus_interface = "org.freedesktop.DBus.Properties",
+			signal_name = "PropertiesChanged",
+			arg0 = DEVICE_INTERFACE,
+			path_keyword = "path")
+
+def device_notify(path, changed):
+	for listener in device_listeners:
+		listener(path, changed)
+
+def service_notify(path, changed):
+	for listener in service_listeners:
+		listener(path, changed)
+
+def characteristic_notify(path, changed):
+	for listener in characteristic_listeners:
+		listener(path, changed)
+
+def interfaces_added(path, interfaces):
+	if DEVICE_INTERFACE in interfaces:
+		device_notify(path, interfaces[DEVICE_INTERFACE])
+	if GATT_SVC_INTERFACE in interfaces:
+		service_notify(path, interfaces[GATT_SVC_INTERFACE])
+	if GATT_CHAR_INTERFACE in interfaces:
+		characteristic_notify(path, interfaces[GATT_CHAR_INTERFACE])
+
+def properties_changed(interface, changed, invalidated, path):
+	if interface != DEVICE_INTERFACE:
+		return
+
+	device_notify(path, changed)
